092ac39a283130b74a98b50ced77fb5383f860b0
sayay <sayay@192.168.1.112>
2018-06-13 16:06:34 +0900

    Breadcrumb bug fix

--- sayaya-ui/src/net/sayaya/ui/widget/Breadcrumb.java
+++ sayaya-ui/src/net/sayaya/ui/widget/Breadcrumb.java
@@ -1,12 +1,11 @@
 package net.sayaya.ui.widget;
 
 import com.google.gwt.dom.client.Element;
+import com.google.gwt.dom.client.Style.Display;
 import com.google.gwt.event.dom.client.ClickEvent;
-import com.google.gwt.user.client.DOM;
 import com.google.gwt.user.client.ui.Anchor;
 import com.google.gwt.user.client.ui.Composite;
-import com.google.gwt.user.client.ui.HasVerticalAlignment;
-import com.google.gwt.user.client.ui.HorizontalPanel;
+import com.google.gwt.user.client.ui.FlowPanel;
 import com.google.gwt.user.client.ui.Widget;
 
 import net.sayaya.ui.activity.Presenter;
@@ -15,7 +14,7 @@ import net.sayaya.ui.place.Place;
 import net.sayaya.ui.style.StyleBreadcrumb;
 
 public class Breadcrumb extends Composite {
-	private final HorizontalPanel layout = new HorizontalPanel();
+	private final FlowPanel layout = new FlowPanel();
 	private final Element splitter;
 	public Breadcrumb() {
 		this(Icon.create(Icon.GSS.chevronDoubleRight()));
@@ -26,8 +25,8 @@ public class Breadcrumb extends Composite {
 	public Breadcrumb(Widget splitter) {
 		initWidget(layout);
 		splitter.addStyleName(StyleBreadcrumb.GSS.splitter());
-		this.splitter = DOM.createTD();
-		this.splitter.appendChild(splitter.getElement().cloneNode(true));
+		this.splitter = (Element) splitter.getElement().cloneNode(true);
+		layout.getElement().getStyle().setDisplay(Display.FLEX);
 	}
 	
 	public Breadcrumb add(Place<?> place) {
@@ -44,9 +43,8 @@ public class Breadcrumb extends Composite {
 	}
 	
 	public Breadcrumb add(Place<?> place, Widget label) {
-		if(layout.getWidgetCount() > 0) layout.getElement().getChild(0).getChild(0).appendChild(splitter.cloneNode(true));
+		if(layout.getWidgetCount() > 0) layout.getElement().appendChild(splitter.cloneNode(true));
 		layout.add(label);
-		layout.setCellVerticalAlignment(label, HasVerticalAlignment.ALIGN_MIDDLE);
 		label.addDomHandler(evt->Presenter.go(place), ClickEvent.getType());
 		label.addStyleName(StyleBreadcrumb.GSS.item());
 		return this;
