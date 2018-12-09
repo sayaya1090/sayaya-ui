182c8f74dab9f163bdac26470f59797346501778
sayaya1090 <sayaya1090@hotmail.com>
2018-12-09 12:37:25 +0900

    ver. 0.5

--- src/net/sayaya/ui/widget/shape/graph/Axis.java
+++ src/net/sayaya/ui/widget/shape/graph/Axis.java
@@ -1,12 +1,19 @@
 package net.sayaya.ui.widget.shape.graph;
 
+import net.sayaya.ui.shape.HasStroke;
 import net.sayaya.ui.widget.shape.impl.ShapeInstance;
 
-public abstract class Axis<T> extends ShapeInstance<Axis<T>> {
+public abstract class Axis<T> extends ShapeInstance<Axis<T>> implements HasStroke {
 	private final AXIS_DIRECTION direction;
 	private final boolean isInverted;
+	private String borderColor;
+	private double borderWidth;
+	private double x, y;
+	private double width;
+	private double height;
+	
 	public Axis(AXIS_DIRECTION direction) {
-		super("polyline");
+		super("path");
 		this.direction = direction;
 		switch(direction) {
 		case X_BOTTOM:
@@ -17,18 +24,16 @@ public abstract class Axis<T> extends ShapeInstance<Axis<T>> {
 			break;
 		case Y_LEFT:
 			isInverted = true;
-	//		setRotate(-Math.PI/2);
+			transform(new Rotate(-Math.PI/2));
 			break;
 		case Y_RIGHT:
 			isInverted = false;
-	//		setRotate(-Math.PI/2);
+			transform(new Rotate(-Math.PI/2));
 			break;
 		default:
 			isInverted = false;
 			break;
 		}
-	//	if(direction == AXIS_DIRECTION.Y_LEFT || direction == AXIS_DIRECTION.Y_RIGHT) setRotate(-Math.PI/2);
-		
 	}
 	public boolean isInverted() {
 		return isInverted;
@@ -39,6 +44,58 @@ public abstract class Axis<T> extends ShapeInstance<Axis<T>> {
 
 	public abstract double map(T data);
 	
+	public double getX() {
+		return x;
+	}
+	
+	public Axis<T> setX(double x) {
+		this.x = x;
+		return this;
+	}
+	
+	public double getY() {
+		return y;
+	}
+	
+	public Axis<T> setY(double y) {
+		this.y = y;
+		return this;
+	}
+	@Override
+	public Axis<T> setBorderWidth(double width) {
+		this.width = width;
+		getElement().setAttribute("stroke-width", String.valueOf(width));
+		return this;
+	}
+
+	@Override
+	public String getBorderColor() {
+		return borderColor;
+	}
+
+	@Override
+	public Axis<T> setBorderColor(String color) {
+		this.borderColor = color;
+		getElement().setAttribute("stroke", color);
+		return this;
+	}
+	public double getWidth() {
+		return width;
+	}
+
+	public Axis<T> setWidth(double width) {
+		this.width = width;
+		return this;
+	}
+
+	public double getHeight() {
+		return height;
+	}
+
+	public Axis<T> setHeight(double height) {
+		this.height = height;
+		return this;
+	}
 	public static enum AXIS_DIRECTION {
 		X_BOTTOM
 		, X_TOP
---
+++ src/net/sayaya/ui/widget/shape/graph/AxisContinuous.java
@@ -0,0 +1,62 @@
+package net.sayaya.ui.widget.shape.graph;
+
+import com.google.gwt.i18n.client.NumberFormat;
+
+import net.sayaya.ui.graph.Axis.AXIS_DIRECTION;
+import net.sayaya.ui.shape.HasStroke;
+
+public class AxisContinuous<T extends Number> extends Axis<T> implements HasStroke {
+	private T min, max;
+	private T step;
+	private NumberFormat nf = NumberFormat.getDecimalFormat();
+	
+	public AxisContinuous(AXIS_DIRECTION direction) {
+		super(direction);
+	}
+	public AxisContinuous<T> setNumberFormat(NumberFormat nf) {
+		this.nf = nf;
+		return this;
+	}
+	
+	public AxisContinuous<T> setMin(T min) {
+		this.min = min;
+		return this;
+	}
+	
+	public AxisContinuous<T> setMax(T max) {
+		this.max = max;
+		return this;
+	}
+	
+	public T getMin() {
+		return min;
+	}
+	
+	public T getMax() {
+		return max;
+	}
+	
+	protected String toString(Double value) {
+		return nf.format(value);
+	}
+	
+	public AxisContinuous<T> setStep(T step) {
+		this.step = step;
+		return this;
+	}
+	
+	public T getStep() {
+		return step;
+	}
+	
+	@Override
+	public double map(T data) {
+		double length = max.doubleValue() - min.doubleValue();
+		double scaleX = getWidth() / length;
+		double translate = -min.doubleValue();
+		if(this.getDirection() == AXIS_DIRECTION.Y_LEFT || this.getDirection() == AXIS_DIRECTION.Y_RIGHT) 
+			return getY()-(data.doubleValue() + translate) * scaleX;
+		else return getX() + (data.doubleValue() + translate) * scaleX;
+	}
+
+}
