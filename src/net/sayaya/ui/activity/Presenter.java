640175cd283311e570e461de26257e1be977c1e8
sayaya1090 <sayaya1090@hotmail.com>
2019-04-16 10:22:05 +0900

    wewe

--- src/net/sayaya/ui/activity/Presenter.java
+++ src/net/sayaya/ui/activity/Presenter.java
@@ -1,6 +1,12 @@
 package net.sayaya.ui.activity;
 
+import java.util.HashSet;
+import java.util.Set;
+
 import com.google.gwt.core.client.GWT;
+import com.google.gwt.event.logical.shared.ValueChangeEvent;
+import com.google.gwt.event.logical.shared.ValueChangeHandler;
+import com.google.gwt.event.shared.HandlerRegistration;
 import com.google.gwt.place.shared.Place;
 import com.google.gwt.place.shared.PlaceController;
 
@@ -8,12 +14,18 @@ public interface Presenter {
 	void goTo(Place place);
 	Place getCurrentPlace();
 	
+	final static Set<ValueChangeHandler<Place>> HANDLERS = new HashSet<>();
 	final static ClientFactory CLIENT_FACTORY = GWT.create(ClientFactory.class);
 	final static Presenter PRESENTER = new PresenterImpl(CLIENT_FACTORY);
 	public static void go(Place place) {
 		PRESENTER.goTo(place);
+		ValueChangeEvent<Place> evt = new ValueChangeEvent<Place>(place) {};
+		HANDLERS.forEach(handler->handler.onValueChange(evt));
 	}
-	
+	public static HandlerRegistration addValueChangeHandler(ValueChangeHandler<Place> handler) {
+		HANDLERS.add(handler);
+		return ()->HANDLERS.remove(handler);
+	}
 	public static class PresenterImpl implements Presenter {
 		private final PlaceController placeController;
 		public PresenterImpl(ClientFactory factory) {
